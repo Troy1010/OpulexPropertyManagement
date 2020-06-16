ba4043892d41be013a749747189a9e68c6d66e51
Troy Swanson <2troy2020@gmail.com>
2020-06-16 12:46:06 -0500

    Toolbar

--- app/src/main/java/com/example/opulexpropertymanagement/ui/ActivityHost.kt
+++ app/src/main/java/com/example/opulexpropertymanagement/ui/ActivityHost.kt
@@ -1,7 +1,23 @@
 package com.example.opulexpropertymanagement.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.opulexpropertymanagement.R
+import kotlinx.android.synthetic.main.activity_host.*
 
-class ActivityHost: AppCompatActivity(R.layout.activity_host) {
+class ActivityHost: AppCompatActivity() {
+//    val navController by lazy { this.findNavController(R.id.fragNavHost) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host)
+        val navController = findNavController(R.id.fragNavHost)
+
+        // Toolbar
+        setSupportActionBar(toolbar_main)
+        setupActionBarWithNavController(navController)
+    }
 }
