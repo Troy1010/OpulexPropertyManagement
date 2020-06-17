5e9b7f73c636fbb1185c542a1f3b8d85fd9314f4
Troy Swanson <2troy2020@gmail.com>
2020-06-16 21:35:43 -0500

    ActivityHost` Toolbar navigates back on back arrow click

--- app/src/main/java/com/example/opulexpropertymanagement/ui/ActivityHost.kt
+++ app/src/main/java/com/example/opulexpropertymanagement/ui/ActivityHost.kt
@@ -1,11 +1,13 @@
 package com.example.opulexpropertymanagement.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.opulexpropertymanagement.R
+import com.example.tmcommonkotlin.logz
 import kotlinx.android.synthetic.main.activity_host.*
 
 class ActivityHost: AppCompatActivity() {
@@ -19,5 +21,14 @@ class ActivityHost: AppCompatActivity() {
         // Toolbar
         setSupportActionBar(toolbar_main)
         setupActionBarWithNavController(navController)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var returning = super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            android.R.id.home -> {
+                this.onBackPressed()
+            }
+        }
+        return returning
     }
 }
