5b64b47bcb7d72064a5c19c9e2e4484d7fafa3cc
Troy Swanson <2troy2020@gmail.com>
2020-06-19 00:39:49 -0500

    minor

--- app/src/main/java/com/example/opulexpropertymanagement/ab_view_models/LoginVM.kt
+++ app/src/main/java/com/example/opulexpropertymanagement/ab_view_models/LoginVM.kt
@@ -3,9 +3,10 @@ package com.example.opulexpropertymanagement.ab_view_models
 import androidx.lifecycle.ViewModel
 import com.example.opulexpropertymanagement.ac_ui.Repo
 import com.example.opulexpropertymanagement.util.convertRXToLiveData
+import com.example.opulexpropertymanagement.util.toLiveData
 
-class LoginVM: ViewModel() {
-    val liveDataTryLogin by lazy { convertRXToLiveData(Repo.streamTryLogin) }
+class LoginVM : ViewModel() {
+    val liveDataTryLogin by lazy { Repo.streamTryLogin.toLiveData() }
     fun tryLogin(email: String, password: String) {
         Repo.tryLogin(email, password)
     }
