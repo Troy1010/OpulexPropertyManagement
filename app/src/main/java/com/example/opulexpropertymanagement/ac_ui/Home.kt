dba44add23fb6f75bca08976ed69e74bc314f357
Troy Swanson <2troy2020@gmail.com>
2020-06-20 20:06:39 -0500

    Firebase Hello World test

--- app/src/main/java/com/example/opulexpropertymanagement/ac_ui/Home.kt
+++ app/src/main/java/com/example/opulexpropertymanagement/ac_ui/Home.kt
@@ -5,12 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.opulexpropertymanagement.R
-import com.example.opulexpropertymanagement.databinding.FragHomeBinding
-import com.example.opulexpropertymanagement.ac_ui.inheritables.OXFragment
 import com.example.opulexpropertymanagement.ab_view_models.GlobalVM
+import com.example.opulexpropertymanagement.ac_ui.inheritables.OXFragment
+import com.example.opulexpropertymanagement.databinding.FragHomeBinding
+import com.google.firebase.database.FirebaseDatabase
+
 
 class Home : OXFragment() {
     lateinit var mBinding: FragHomeBinding
@@ -36,6 +37,13 @@ class Home : OXFragment() {
         }
         mBinding.btnPrintSomething.setOnClickListener {
             GlobalVM.printSomething()
+        }
+        mBinding.btnDoSomethingTwo.setOnClickListener {
+            // Write a message to the database
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
         }
         return mBinding.root
     }
