9a1b10e085e6bd5ee08d02bd8b94679553ef73d2
Troy Swanson <2troy2020@gmail.com>
2020-06-24 11:02:59 -0500

    removed unused UserState

--- app/src/main/java/com/example/opulexpropertymanagement/models/UserState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.opulexpropertymanagement.ac_ui
-
-data class UserState(
-    val wantsLogin:Boolean = false,
-    val hasLogin:Boolean = false,
-    val user: User?=null,
-    val errorReason:String?=null
-)
