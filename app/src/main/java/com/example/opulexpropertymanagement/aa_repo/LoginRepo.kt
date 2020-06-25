87a4c5743b43e26e3c5a93698299c7ed71e87562
Troy Swanson <2troy2020@gmail.com>
2020-06-25 00:43:56 -0500

    login button greys out

--- app/src/main/java/com/example/opulexpropertymanagement/aa_repo/LoginRepo.kt
+++ app/src/main/java/com/example/opulexpropertymanagement/aa_repo/LoginRepo.kt
@@ -1,10 +1,18 @@
 package com.example.opulexpropertymanagement.aa_repo
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.opulexpropertymanagement.ac_ui.GlobalRepo
+import com.example.opulexpropertymanagement.models.streamable.TryLoginResult
+import com.example.tmcommonkotlin.logz
 
 class LoginRepo {
-    val streamLoginAttemptOverloadTimestamp = MutableLiveData<Int>()
+    val streamLoginAttemptOverloadTimestamp = MediatorLiveData<Long>()
     init {
-        // load email:timestamp map from db
+        streamLoginAttemptOverloadTimestamp.addSource(GlobalRepo.streamLoginAttemptResult) {
+            if (it is TryLoginResult.Failure.TooManyAttempts) {
+                streamLoginAttemptOverloadTimestamp.value = System.currentTimeMillis()/1000
+            }
+        }
     }
 }
--- app/src/main/java/com/example/opulexpropertymanagement/ab_view_models/LoginVM.kt
+++ app/src/main/java/com/example/opulexpropertymanagement/ab_view_models/LoginVM.kt
@@ -3,13 +3,24 @@ package com.example.opulexpropertymanagement.ab_view_models
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.opulexpropertymanagement.aa_repo.LoginRepo
 import com.example.opulexpropertymanagement.ac_ui.GlobalRepo
 import com.example.opulexpropertymanagement.models.streamable.TryLoginResult
+import com.example.tmcommonkotlin.logv
+import com.example.tmcommonkotlin.logz
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginVM : ViewModel() {
     fun tryLogin() {
-        globalRepo.tryLogin(enteredEmail.value, enteredPassword.value)
+        if (isLockedOut.value!=true) {
+            globalRepo.tryLogin(enteredEmail.value, enteredPassword.value)
+        } else {
+            globalRepo.streamLoginAttemptResult.value = TryLoginResult.Failure.TooManyAttempts
+        }
     }
 
     val globalRepo = GlobalRepo
@@ -20,7 +31,17 @@ class LoginVM : ViewModel() {
     val isLockedOut = MediatorLiveData<Boolean>()
     init {
         isLockedOut.addSource(loginRepo.streamLoginAttemptOverloadTimestamp) {
-            isLockedOut.value = System.currentTimeMillis() - it > 300
+            val timeSinceAttempt = (System.currentTimeMillis()/1000) - it
+            val totalTimeToWait = 10
+            isLockedOut.value = timeSinceAttempt < totalTimeToWait
+            if (isLockedOut.value==true) {
+                viewModelScope.launch {
+                    delay((totalTimeToWait-timeSinceAttempt+1)*1000)
+                    withContext(viewModelScope.coroutineContext + Dispatchers.Main) {
+                        loginRepo.streamLoginAttemptOverloadTimestamp.value = loginRepo.streamLoginAttemptOverloadTimestamp.value
+                    }
+                }
+            }
         }
     }
 }
