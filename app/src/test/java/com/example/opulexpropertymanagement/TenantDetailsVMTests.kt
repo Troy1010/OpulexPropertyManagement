8231d4bc939282a66d0778971f58f89fa118f11e
Troy Swanson <2troy2020@gmail.com>
2020-07-14 23:50:56 -0500

    trying to do JUnit4 + Parameterized Test

--- app/src/test/java/com/example/opulexpropertymanagement/TenantDetailsVMTests.kt
+++ app/src/test/java/com/example/opulexpropertymanagement/TenantDetailsVMTests.kt
@@ -2,8 +2,6 @@ package com.example.opulexpropertymanagement
 
 import android.net.Uri
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.viewModelScope
 import com.example.opulexpropertymanagement.layers.data_layer.Repo
 import com.example.opulexpropertymanagement.layers.view_models.TenantDetailsVM
 import com.example.opulexpropertymanagement.models.Document
@@ -13,24 +11,25 @@ import com.example.opulexpropertymanagement.zzTestUtil.MyCoroutinesRule
 import com.example.opulexpropertymanagement.zzTestUtil.getOrAwaitValue
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
-import kotlinx.coroutines.*
+import junitparams.JUnitParamsRunner
+import junitparams.NamedParameters
+import junitparams.Parameters
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.mockito.ArgumentMatchers.anyString
-import org.mockito.ArgumentMatchers.same
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnit
 
+
 @ExperimentalCoroutinesApi
-@RunWith(JUnit4::class)
-class TenantDetailsVMTests {
+@RunWith(JUnitParamsRunner::class)
+open class TenantDetailsVMTests {
     @get:Rule
     val liveDataRule = InstantTaskExecutorRule()
     @get:Rule
@@ -39,21 +38,16 @@ class TenantDetailsVMTests {
     val initializeMocksRule = MockitoJUnit.rule()
     @Mock
     lateinit var repo: Repo
-    lateinit var document: Document
-    lateinit var tenant: Tenant
+    val document = Document("654", "795", "A good doc")
+    val tenant = Tenant(
+        "123456", "123", "778", "123 Fake St",
+        "GoodCarrot@gmail.com", "111-222-3333", "Bob"
+    )
     val uri = Uri.parse("content://com.android.providers.media.documents/document/image%3A40")
 
-    @Before
-    fun setUp() {
-        tenant = Tenant(
-            "123456", "123", "778", "123 Fake St",
-            "GoodCarrot@gmail.com", "111-222-3333", "Bob"
-        )
-        document = Document("654", "795", "A good doc")
-    }
-
     @Test
-    fun `test addDocument`() = coroutineRule.testDispatcher.runBlockingTest {
+    @Parameters(method = "testAddDocumentValues")
+    fun testAddDocument(age:Int, b:Boolean) = coroutineRule.testDispatcher.runBlockingTest {
         // given
         Mockito.`when`(repo.addDocument(anyString(), any(), anyString()))
             .doReturn(AddDocumentResult.Success(document))
@@ -62,6 +56,11 @@ class TenantDetailsVMTests {
         tenantDetailsVM.addDocument(uri, "A good document")
         val actual = tenantDetailsVM.addDocumentResult.getOrAwaitValue()
         // assert
+        println("i:$age, s:$b")
         Assert.assertNotNull(actual)
+    }
+    @NamedParameters("grownups")
+    open fun testAddDocumentValues(): Array<Any>? {
+        return arrayOf(arrayOf(17, false), arrayOf(22, true))
     }
 }
